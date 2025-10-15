f0054dfb0e072219a79eb8d08f0643d6688735ca
kathroju-srivardhini <vardhinikatroju@gmail.com>
2025-10-15 20:33:54 +0530

    Added Java basics folder

---
+++ Java-basics/First.java
@@ -0,0 +1,10 @@
+public class First{
+    public static void main(String[] args){
+        //This is my first program in Java
+        /*Hi
+         * hello
+         * welcome to my repo
+         */
+        System.out.println("I am a passionate computer science student aiming to build my career in tech.");
+    }
+}
