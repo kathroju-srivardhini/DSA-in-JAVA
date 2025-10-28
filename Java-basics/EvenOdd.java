7bc45ba791311478da931d71605eb5ec283c9c4e
kathroju-srivardhini <vardhinikatroju@gmail.com>
2025-10-28 19:00:04 +0530

    Added new file

---
+++ Java-basics/EvenOdd.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class EvenOdd {
+    public static void main(String[] args){
+            System.out.print("Enter a number : ");
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            if(n%2==0){
+                System.out.printf("\n%d is a even number",n);
+            }
+            else
+                System.out.printf("\n%d is an odd number",n);
+    }
+}
