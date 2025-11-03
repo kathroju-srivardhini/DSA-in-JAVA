aa3ced110ff0f1b8be0bf64765b5f33095f02913
kathroju-srivardhini <vardhinikatroju@gmail.com>
2025-11-03 12:07:07 +0530

    deleted class files

---
+++ Java-basics/Number Patterns/Pattern3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Pattern3{
+    public static void main(String[] args){
+        System.out.print("Enter the number of rows : ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=n;i>0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+/*Output:
+ Enter the number of rows : 5
+1 2 3 4 5 
+1 2 3 4
+1 2 3
+1 2
+1
+ */
---
+++ Java-basics/Number Patterns/Pattern4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Pattern4{
+    public static void main(String[] args){
+        System.out.print("Enter the number of rows : ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(k+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
