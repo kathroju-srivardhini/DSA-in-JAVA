acab5ff676db22c15014ad9b8a0190120b9b962d
kathroju-srivardhini <vardhinikatroju@gmail.com>
2025-11-16 12:58:18 +0530

    Added Fibonacci Series and Prime numbers programs

---
+++ OOPS in Java/FibonacciSeries.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+class fibonacci{
+    int i=0,a=0,b=1,c;
+    public void iterative(int n){
+        if(n==0){
+            System.out.print("Fibonacci Series using iterative method : ");
+
+        }
+        else if (n==1){
+            System.out.print("Fibonacci Series using iterative method : "+a);
+        }
+        else{
+            System.out.print("Fibonacci Series using iterative method : "+a+" "+b);
+            for(i=1;i<n-1;i++){
+                c=a+b;
+                a=b;
+                b=c;
+                System.out.print(" "+c);
+            }
+        }
+    }
+    public int recursive(int n){
+        if(n==0){
+            return 0; // 1st number in the sequence
+        }
+        else if(n==1){
+            return n; //2nd number in the sequence
+        }
+        else{
+            return recursive(n-1)+recursive(n-2); //Sum of last two numbers in the sequence
+        }
+
+    }
+}
+public class FibonacciSeries{
+    public static void main(String[] args){
+        System.out.print("Enter how many numbers do you want in the sequence : ");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        fibonacci fib=new fibonacci();
+        fib.iterative(num);
+        System.out.print("\nFibonacci Series using recursion method : ");
+        for(int i=0;i<num;i++){ //starts the process from 0 as 1st number in the sequence and goes up to num numbers
+            System.out.print(fib.recursive(i)+" ");
+        }
+        sc.close();
+    }
+}
+/* OUTPUT:
+Enter how many numbers do you want in the sequence : 12
+Fibonacci Series using iterative method : 0 1 1 2 3 5 8 13 21 34 55 89
+Fibonacci Series using recursion method : 0 1 1 2 3 5 8 13 21 34 55 89 
+*/
---
+++ OOPS in Java/PrimeNumbers.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Prime{
+    public boolean p(int n){
+//We know that factors are in pairs, in each pair the smaller number is always less than root n. So we just need to check divisibility only upto root n.
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return false;
+            }    
+        }
+        return true ;
+     }
+    }
+public class PrimeNumbers{
+    public static void main(String[] args){
+        System.out.print("Enter the limit upto which you want prime numbers : ");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        Prime obj=new Prime();
+        if(num==0||num==1){
+        System.out.print("There are no prime numbers in this limit.");
+        }
+        else{ 
+        System.out.printf("Prime numbers upto %d are : ",num);
+        for(int i=2;i<=num;i++){
+            boolean k=obj.p(i);
+            if(k){ 
+                System.out.print(i +" ");
+            }
+        }
+        sc.close();
+    }
+ }
+}
+/* Output:
+Enter the limit upto which you want prime numbers : 100
+Prime numbers upto 100 are : 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 83 89 97
+*/
